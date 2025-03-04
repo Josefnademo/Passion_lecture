e9b3179da8db646a182aa754a477dffde4e1c3cd
C-HM <pc01zcg@eduvaud.ch>
2025-03-04 15:46:54 +0100

    update(models)
    add Freezename

--- code/src/model/EvaluateModel.js
+++ code/src/model/EvaluateModel.js
@@ -1,21 +1,25 @@
 const EvaluateModel = (sequelize, DataTypes) => {
-  return sequelize.define("Evaluer", {
-    commentaire: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    note: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      min: {
-        args: [1.0],
-        msg: "la note doit être au moins de 1",
+  return sequelize.define(
+    "t_evaluer",
+    {
+      commentaire: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
-      max: {
-        args: [5.0],
-        msg: "la note ne peux pas dépasser 5",
+      note: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        min: {
+          args: [1.0],
+          msg: "la note doit être au moins de 1",
+        },
+        max: {
+          args: [5.0],
+          msg: "la note ne peux pas dépasser 5",
+        },
       },
     },
-  });
+    { freezeTableName: true }
+  );
 };
 export { EvaluateModel };
