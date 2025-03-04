c8331523e3b6db68aa79a66d9cddb02942a413e7
C-HM <pc01zcg@eduvaud.ch>
2025-03-04 15:50:27 +0100

    Create mock-evaluate.js
    Creation du mock evaluate

---
+++ code/src/db/mock-evaluate.js
@@ -0,0 +1,15 @@
+let evaluations = [
+  {
+    commentaire: "Elle très mauvais à lire",
+    note: 3,
+  },
+  {
+    commentaire: "north korea forever",
+    note: 5,
+  },
+  {
+    commentaire: "Amazing American JOKE!",
+    note: 10,
+  },
+];
+export { evaluations };
